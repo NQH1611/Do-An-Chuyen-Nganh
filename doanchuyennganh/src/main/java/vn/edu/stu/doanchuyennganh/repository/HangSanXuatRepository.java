7a46c337931663e85cc18684bb1475a5f59f7285
NQH1611 <quochuy.140101@gmail.com>
2022-12-14 18:14:53 +0700

    create repository hangsanxuat

---
+++ doanchuyennganh/src/main/java/vn/edu/stu/doanchuyennganh/repository/HangSanXuatRepository.java
@@ -0,0 +1,9 @@
+package vn.edu.stu.doanchuyennganh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import vn.edu.stu.doanchuyennganh.model.HangSanXuat;
+
+public interface HangSanXuatRepository extends JpaRepository<HangSanXuat, Integer>{
+    
+}
