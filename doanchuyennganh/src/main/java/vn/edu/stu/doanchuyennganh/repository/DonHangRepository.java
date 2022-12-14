37bf86b42f8577e68e799c70b48d7c4cb63ddec0
NQH1611 <quochuy.140101@gmail.com>
2022-12-14 18:12:23 +0700

    create repository donhang

---
+++ doanchuyennganh/src/main/java/vn/edu/stu/doanchuyennganh/repository/DonHangRepository.java
@@ -0,0 +1,9 @@
+package vn.edu.stu.doanchuyennganh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import vn.edu.stu.doanchuyennganh.model.DonHang;
+
+public interface DonHangRepository extends JpaRepository<DonHang, Integer>{
+    
+}
