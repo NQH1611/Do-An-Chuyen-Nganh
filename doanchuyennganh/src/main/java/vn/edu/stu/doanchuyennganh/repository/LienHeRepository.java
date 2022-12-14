6ae6f5746f60164604f7e615b057e94e40d4d458
NQH1611 <quochuy.140101@gmail.com>
2022-12-14 18:15:56 +0700

    create repository lienhe

---
+++ doanchuyennganh/src/main/java/vn/edu/stu/doanchuyennganh/repository/LienHeRepository.java
@@ -0,0 +1,9 @@
+package vn.edu.stu.doanchuyennganh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import vn.edu.stu.doanchuyennganh.model.LienHe;
+
+public interface LienHeRepository extends JpaRepository<LienHe, Integer>{
+    
+}
