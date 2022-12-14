ba3ca51081d1af71c51d6772795c83c9ae63494f
NQH1611 <quochuy.140101@gmail.com>
2022-12-14 18:17:34 +0700

    create repository sanpham

---
+++ doanchuyennganh/src/main/java/vn/edu/stu/doanchuyennganh/repository/SanPhamRepository.java
@@ -0,0 +1,9 @@
+package vn.edu.stu.doanchuyennganh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import vn.edu.stu.doanchuyennganh.model.SanPham;
+
+public interface SanPhamRepository extends JpaRepository<SanPham, Integer>{
+    
+}
